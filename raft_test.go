8a0e862fc30eb6736297102b7433ef079f8669d3
hslam <791874158@qq.com>
2020-12-29 22:47:59 +0800

    add more test

--- raft_test.go
+++ raft_test.go
@@ -159,5 +159,115 @@ func TestCluster(t *testing.T) {
 	}
 	wg.Wait()
 	os.RemoveAll(dir)
+}
+
+func TestClusterMore(t *testing.T) {
+	dir := "raft.test"
+	os.RemoveAll(dir)
+	infos := []*NodeInfo{{Address: "localhost:9001"}, {Address: "localhost:9002"}, {Address: "localhost:9003"}, {Address: "localhost:9004"}, {Address: "localhost:9005"}}
+	wg := sync.WaitGroup{}
+	var readflag uint32
+	startRead := make(chan struct{})
+	var joinflag uint32
+	startJoin := make(chan struct{})
+	al := sync.WaitGroup{}
+	al.Add(3)
+	for i := 0; i < len(infos); i++ {
+		address := infos[i].Address
+		index := i
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			ctx := &testContext{data: ""}
+			strs := strings.Split(address, ":")
+			port, _ := strconv.Atoi(strs[1])
+			var n Node
+			var err error
+			if index < 3 {
+				n, err = NewNode(strs[0], port, dir+"/node."+strconv.FormatInt(int64(index), 10), ctx, false, infos[:3])
+				if err != nil {
+					t.Error(err)
+				}
+			} else {
+				<-startJoin
+				n, err = NewNode(strs[0], port, dir+"/node."+strconv.FormatInt(int64(index), 10), ctx, true, infos[:index+1])
+				if err != nil {
+					t.Error(err)
+				}
+			}
+			node := n.(*node)
+			node.RegisterCommand(&testCommand{})
+			node.SetCodec(&JSONCodec{})
+			node.SetSnapshot(&testSnapshot{ctx: ctx})
+			node.SetSnapshotPolicy(Always)
+			node.SetSnapshotPolicy(EverySecond)
+			node.SetSnapshotPolicy(EveryMinute)
+			node.SetSnapshotPolicy(EveryHour)
+			node.SetSnapshotPolicy(EveryDay)
+			node.SetSnapshotPolicy(DefalutSync)
+			node.SetSnapshotPolicy(Never)
+			node.ClearSyncType()
+			node.AppendSyncType(1, 1)
+			node.SetGzipSnapshot(true)
+			node.LeaderChange(func() {
+			})
+			node.MemberChange(func() {
+			})
+			node.Start()
+			node.LeaderChange(func() {
+				time.Sleep(time.Second * 2)
+				lookupLeader := node.LookupPeer(leader)
+				leader := node.Leader()
+				if lookupLeader != nil && lookupLeader.Address != leader {
+					t.Error(lookupLeader.Address, leader)
+				}
+				node.Do(&testCommand{"foobar"})
+				if node.IsLeader() {
+					if atomic.CompareAndSwapUint32(&readflag, 0, 1) {
+						close(startRead)
+					}
+				}
+				<-startRead
+				time.Sleep(time.Second)
+				if ok := node.Lease(); ok {
+					value := ctx.Get()
+					if value != "foobar" {
+						t.Error(value)
+					}
+				}
+				if ok := node.ReadIndex(); ok {
+					value := ctx.Get()
+					if value != "foobar" {
+						t.Error(value)
+					}
+				}
+				if node.IsLeader() {
+					if atomic.CompareAndSwapUint32(&joinflag, 0, 1) {
+						close(startJoin)
+					}
+				}
+			})
+			if index < 3 {
+				<-startJoin
+				time.Sleep(time.Second)
+				if node.IsLeader() {
+					if _, ok := node.Leave(infos[4].Address); !ok {
+						t.Error()
+					}
+					al.Done()
+				} else {
+					if _, ok := node.Leave(infos[3].Address); !ok {
+						t.Error()
+					}
+					al.Done()
+				}
+			}
+			al.Wait()
+			time.Sleep(time.Second * 2)
+			node.Stop()
+		}()
+	}
+	wg.Wait()
+	os.RemoveAll(dir)
 	GetLogLevel()
 }
